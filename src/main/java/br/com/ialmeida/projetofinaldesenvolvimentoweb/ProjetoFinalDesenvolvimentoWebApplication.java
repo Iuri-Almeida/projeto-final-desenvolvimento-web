e9406c9294ef6e13b3535dd0b1f877e5e47e7973
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-03-12 18:26:33 -0300

    Project created

---
+++ src/main/java/br/com/ialmeida/projetofinaldesenvolvimentoweb/ProjetoFinalDesenvolvimentoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.projetofinaldesenvolvimentoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalDesenvolvimentoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFinalDesenvolvimentoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ialmeida/projetofinaldesenvolvimentoweb/ProjetoFinalDesenvolvimentoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.projetofinaldesenvolvimentoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalDesenvolvimentoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
